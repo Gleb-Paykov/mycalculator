0f978cbddd72562a58dc643c0a50c6682dc07b64
Gleb-Paykov <78311058+Gleb-Paykov@users.noreply.github.com>
2021-01-31 20:31:32 +0500

    Add files via upload

---
+++ project12.py
@@ -0,0 +1,38 @@
+import math
+
+while True:
+    def calc(number1, number2):
+        opr = input("Что будем делать (+, -, *, **, /, //, %, fac, ceil, sqrt) ")
+
+        if opr == "+":
+            print(number1 + number2)
+
+        elif opr == "-":
+            print(number1 - number2)
+
+        elif opr == "*":
+            print(number1 * number2)
+
+        elif opr == "**":
+            print(number1 ** number2)
+
+        elif opr == "/":
+            print(number1 / number2)
+
+        elif opr == "//":
+            print(number1 // number2)
+
+        elif opr == "%":
+            print(number1 % number2)
+
+        elif opr == "fac":
+            print(math.factorial(number1))
+
+        elif opr == "ceil":
+            print(math.ceil(number1))
+
+        elif opr == "sqrt":
+            print(math.sqrt(number1))
+    number1 = int(input("Введите первое число "))
+    number2 = int(input("Введите второе число "))
+    calc(number1, number2)
